808c95f4a14fa0b18ba24f7065302606723e1e83
Carlos Oliva <coliva@unab.edu.co>
2025-10-15 15:22:13 -0500

    Basic Navigation

--- app/src/main/java/me/carlosoliva/unabshop/LoginScreen.kt
+++ app/src/main/java/me/carlosoliva/unabshop/LoginScreen.kt
@@ -112,7 +112,9 @@ fun LoginScreen(navController: NavController) {
             Spacer(modifier = Modifier.height(24.dp))
             // Botón de Iniciar Sesión
             Button(
-                onClick = { },
+                onClick = {
+                    navController.navigate(route = "home")
+                },
                 modifier = Modifier
                     .fillMaxWidth()
                     .height(50.dp),
